3257040d54663eab8a6a0cd863abffefa95e2b06
sai <sai.kumar.dev1999@gmail.com>
2024-10-04 22:47:29 +0530

    eslint react hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,17 @@
-const prettier = require('eslint-plugin-prettier');
+import mantine from 'eslint-config-mantine';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-module.exports = [
+export default tseslint.config(
+  ...mantine,
+
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
   {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
     plugins: {
-      prettier,
+      'react-hooks': reactHooks,
     },
     rules: {
-      'prettier/prettier': 'error',
-      semi: ['warn', 'always'],
-      'no-duplicate-imports': 'error',
-      'react/jsx-fragments': ['error', 'element'],
-      'react/self-closing-comp': 'error',
-      'react/no-array-index-key': 'error',
-      'react/no-unused-state': 'error',
-      'react/no-unused-prop-types': 'error',
+      ...reactHooks.configs.recommended.rules,
     },
-  },
-];
+  }
+);
