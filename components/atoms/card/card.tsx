26f5c76e2227fa9cab6338e688e262af246f02cb
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-12-17 18:26:50 +0530

    ...changes complete

--- components/atoms/card/card.tsx
+++ components/atoms/card/card.tsx
@@ -6,7 +6,7 @@ import { theme } from '@/constants/theme';
 export interface CardProps {
   data: {
     _id: string,
-    imageSource: string;
+    image: string;
     title: string;
     description?: string;
   },
@@ -23,7 +23,7 @@ const Card: React.FC<CardProps> = ({ data, onPress, cardStyle={}, imageContainer
   return (
     <TouchableOpacity style={[styles.card, cardStyle]} onPress={onPress}>
       <View style={imageContainerStyle}>
-        <Image source={{uri: data.imageSource}} style={[styles.cardImage, imageStyle]} />
+        <Image source={{uri: data.image}} style={[styles.cardImage, imageStyle]} />
       </View>
       <View
         style={[textContainerStyle, styles.textContainer]}
