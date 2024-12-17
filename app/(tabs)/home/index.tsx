90d100fa61e3db8b44de25de38d61c83233f09f3
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-12-17 18:24:00 +0530

    card schema rectification, to match web schema for sanity

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -5,17 +5,17 @@ const hotelsData = [
   {
       _id: '1',
       title: 'The lead',
-      imageSource: { uri: 'https://thumbs.dreamstime.com/b/luxury-hotel-room-master-bedroom-creative-ai-design-background-instagram-facebook-wall-painting-photo-wallpaper-backgrounds-325040660.jpg' },
+      image: 'https://thumbs.dreamstime.com/b/luxury-hotel-room-master-bedroom-creative-ai-design-background-instagram-facebook-wall-painting-photo-wallpaper-backgrounds-325040660.jpg',
   },
   {
       _id: '2',
       title: 'Prestige',
-      imageSource: { uri: 'https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI=' }
+      image: 'https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI='
   },
   {
       _id: '3',
       title: 'Marriott',
-      imageSource: { uri: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/620719916.webp?k=616cefe723433fd501f4fe89c7f415ce49822b10d769c7a725f8e35b39be66af&o=' }
+      image: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/620719916.webp?k=616cefe723433fd501f4fe89c7f415ce49822b10d769c7a725f8e35b39be66af&o='
   }
 ]
 
