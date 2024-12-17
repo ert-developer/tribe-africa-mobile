f7d2a478b0a977e3d2c3d8c150a417c5cac44331
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-12-17 13:08:40 +0530

    Business screen branch built with initialization of header and segmented control.

---
+++ app/(tabs)/business/business-screen.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native";
+
+const BusinessScreen = () => {
+  return (
+    <View>
+      <Text>Business Screen</Text>
+    </View>
+  )
+};
+
+export default BusinessScreen;
